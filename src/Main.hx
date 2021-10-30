8b1938985407fcb3bf22085ac66f2e15876311cb
Ben Teichman <ben.l.teichman@gmail.com>
2021-10-30 18:51:20 -0400

    feat: working directional controls

--- src/Main.hx
+++ src/Main.hx
@@ -1,10 +1,10 @@
-import hxsl.Types.Matrix;
-
 var numberOfChunks = 64;
 var worldSize = 256;
 
 class Main extends hxd.App {
 	var model:h3d.scene.Object;
+	var debug:h2d.Text;
+	var debug2:h2d.Text;
 
 	override function init() {
 		s3d.lightSystem.ambientLight.set(0.4, 0.4, 0.4);
@@ -19,34 +19,47 @@ class Main extends hxd.App {
 
 		new h3d.scene.CameraController(5, s3d).loadFromCamera();
 
+		var font = hxd.res.DefaultFont.get();
+
+		debug = new h2d.Text(font, s2d);
+		debug2 = new h2d.Text(font, s2d);
+		debug.x = debug.y = debug2.x = 5;
+		debug2.y = 35;
+
 		cache.dispose();
 	}
 
 	override function update(dt:Float) {
 		var direc = model.getLocalDirection();
-		var pos = model.getRelPos(s3d).getPosition();
+		var pos = model.getRelPos(model.parent).getPosition();
 
 		if (hxd.Key.isDown(hxd.Key.UP)) {
 			var move = direc.clone();
-			move.w = 5 * dt;
+			move.scale(2 * dt);
 
-			pos.add(move);
-
-			model.setPosition(move.x, pos.y, pos.z);
+			var res = pos.add(move);
+			model.setPosition(res.x, res.y, res.z);
 		}
 
-		if (hxd.Key.isDown(hxd.Key.DOWN))
-			model.y -= 5 * dt;
+		if (hxd.Key.isDown(hxd.Key.DOWN)) {
+			var move = direc.clone();
+			move.scale(-2 * dt);
+
+			var res = pos.add(move);
+			model.setPosition(res.x, res.y, res.z);
+		}
 
 		if (hxd.Key.isDown(hxd.Key.RIGHT)) {
-			direc.transform(Matrix.R(0, 0, 1.5 * dt));
+			direc.transform(h3d.Matrix.R(0, 0, 1.5 * dt));
 			model.setDirection(direc);
 		}
 
 		if (hxd.Key.isDown(hxd.Key.LEFT)) {
-			direc.transform(Matrix.R(0, 0, -1.5 * dt));
+			direc.transform(h3d.Matrix.R(0, 0, -1.5 * dt));
 			model.setDirection(direc);
 		}
+
+		debug.text = "Position: x=" + model.x + " y=" + model.y + " z=" + model.z;
 	}
 
 	static function main() {
