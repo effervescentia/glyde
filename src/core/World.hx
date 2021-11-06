af165a3b8cb225bf4aef785331bc0b2840c9d0dc
Ben Teichman <ben.l.teichman@gmail.com>
2021-11-06 02:24:18 -0400

    refactor: remove unused

--- src/core/World.hx
+++ src/core/World.hx
@@ -7,15 +7,11 @@ class World extends h3d.scene.World {
 	public static var CHUNK_COUNT = 64;
 	public static var WORLD_SIZE = 128;
 
-	private var powerup:WorldModel;
-
 	public function new(s3d:Scene) {
 		super(CHUNK_COUNT, WORLD_SIZE, s3d);
 	}
 
 	public function populate() {
-		powerup = loadModel(hxd.Res.powerup);
-
 		done();
 	}
 }
